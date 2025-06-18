33943f409be4a8a8c15c2b53c89be27e5c618972
Danny Faber <danny.faber1982@gmail.com>
2025-06-18 23:07:32 +0200

    Changed return type for methodA2 in ClassA

--- src/nl/ou/refd/test/analysis/detectors/doubledefinition/ClassA.java
+++ src/nl/ou/refd/test/analysis/detectors/doubledefinition/ClassA.java
@@ -3,5 +3,5 @@ package nl.ou.refd.test.analysis.detectors.doubledefinition;
 public class ClassA {
 	
 	public void methodA1() {}
-	private void methodA2(int p1A2, long p2A2) {}
+	private int methodA2(int p1A2, long p2A2) { return 0; }
 }
